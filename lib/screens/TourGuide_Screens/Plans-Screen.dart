274e8a40c87e50a352daf431ac4ff4b773c1708e
Eii910 <62373448+Eii910@users.noreply.github.com>
2021-11-29 15:35:51 +0300

    Payment backend still working on

--- lib/screens/TourGuide_Screens/Plans-Screen.dart
+++ lib/screens/TourGuide_Screens/Plans-Screen.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_braintree/flutter_braintree.dart';
+import 'package:http/http.dart' as http;
 
 class PlansScreen extends StatefulWidget {
   @override
@@ -8,6 +12,7 @@ class PlansScreen extends StatefulWidget {
 class _PlansScreenState extends State<PlansScreen> {
   int selectedPlan = 1;
   int amount = 50;
+  var url = 'https://us-central1-athar-654db.cloudfunctions.net/paypalPayment';
 
   @override
   Widget build(BuildContext context) {
@@ -45,7 +50,7 @@ class _PlansScreenState extends State<PlansScreen> {
                       onTap: () {
                         setState(() {
                           selectedPlan = 1;
-                          amount = 50;
+                          amount = 10;
                         });
                       },
                       child: Padding(
@@ -79,7 +84,7 @@ class _PlansScreenState extends State<PlansScreen> {
                                   ),
                                 ),
                                 Text(
-                                  '50 SR',
+                                  "\$10",
                                   style: TextStyle(
                                     color: Color(0xFFF2945E),
                                     fontSize: 22,
@@ -96,7 +101,7 @@ class _PlansScreenState extends State<PlansScreen> {
                       onTap: () {
                         setState(() {
                           selectedPlan = 2;
-                          amount = 100;
+                          amount = 30;
                         });
                       },
                       child: Padding(
@@ -130,7 +135,7 @@ class _PlansScreenState extends State<PlansScreen> {
                                   ),
                                 ),
                                 Text(
-                                  '100 SR',
+                                  "\$30",
                                   style: TextStyle(
                                     color: Color(0xFFF2945E),
                                     fontSize: 22,
@@ -147,7 +152,7 @@ class _PlansScreenState extends State<PlansScreen> {
                       onTap: () {
                         setState(() {
                           selectedPlan = 3;
-                          amount = 300;
+                          amount = 60;
                         });
                       },
                       child: Padding(
@@ -181,7 +186,7 @@ class _PlansScreenState extends State<PlansScreen> {
                                   ),
                                 ),
                                 Text(
-                                  '300 SR',
+                                  "\$60",
                                   style: TextStyle(
                                     color: Color(0xFFF2945E),
                                     fontSize: 22,
@@ -203,9 +208,32 @@ class _PlansScreenState extends State<PlansScreen> {
                               borderRadius: new BorderRadius.circular(20.0),
                             ),
                           ),
-                          onPressed: () {
-                            print(selectedPlan);
-                            print(amount);
+                          onPressed: () async {
+                            var request = BraintreeDropInRequest(
+                              tokenizationKey:
+                                  "sandbox_38dhwgr9_jtbybby5fv5w33hx",
+                              collectDeviceData: true,
+                              paypalRequest: BraintreePayPalRequest(
+                                amount: amount.toString(),
+                                displayName: 'Athar',
+                              ),
+                            );
+
+                            BraintreeDropInResult result =
+                                await BraintreeDropIn.start(request);
+                            if (result != null) {
+                              print(result.paymentMethodNonce.description);
+                              print(result.paymentMethodNonce.nonce);
+
+                              final http.Response response = await http.post(
+                                  Uri.tryParse(
+                                      '$url?payment_method_nonce=${result.paymentMethodNonce}&device_data=${result.deviceData}'));
+
+                              final payResult = jsonDecode(response.body);
+                              if (payResult['result'] == 'success') {
+                                print('payment done');
+                              }
+                            }
                           },
                           child: Container(
                             height: 60,
